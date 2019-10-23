2b96ac1d57c486cc622081bdcf5f2edd8608c76f
dominicdorhat <johnfruit099@gmail.com>
2019-10-23 17:45:38 +0100

    Started task2

---
+++ task2.c
@@ -0,0 +1,29 @@
+// consumer producer problem
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include "coursework.h"
+#include "linkedlist.h"
+
+// consumer
+void * consumer(void * p) {
+    sem_wait(&sync);
+// 
+    items--;
+    temp = 
+// 
+}
+
+// producer
+void * producer() {
+
+}
+
+int main() {
+    pthread_t tid1, tid2;
+    
+
+}
+
