d1c6f9d380f48bbd1925a90841fe35a0dd03e8ff
natsning <hfyst1@bann.cs.nott.ac.uk>
2019-10-25 18:44:53 +0100

    skeleton for task3

--- task3sn.c
+++ task3sn.c
@@ -1,4 +1,4 @@
-// consumer producer problem
+// Bounded consumer producer problem
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,9 +7,9 @@
 #include "coursework.h"
 #include "linkedlist.h"
 
-sem_t sync, delay_consumer;
+sem_t sync,full,empty;
 
-int items, syncVal, delayVal;
+int items, syncVal, fullVal, emptyVal;
 
 void visualise() {
 	printf("iIndex = %d\n", items);
@@ -25,11 +25,11 @@ void * consumer(void * p) {
 	for (int i = 0; i < MAX_NUMBER_OF_JOBS; i++) {
 	
 		sem_wait(&sync);		
-		
+		//
 		// critical section
-		items--;
-		temp = items;
 		
+		//REMOVE STARS BY REMOVEFIRST OF LINKED LIST
+		items--;
 		visualise(); 		
 		// end of critical section
 		sem_post(&sync);
@@ -47,11 +47,11 @@ void * consumer(void * p) {
 void * producer() {
 	
 	// produces job
-	for (int i = 0; i < MAX_NUMBER_OF_JOBS; i++) {
+	while(1){
 
 		// critical section
 		sem_wait(&sync);		
-
+		//GENERATE STARS INTO NODE + ADD NODE TO LAST OF LINKED LIST
 		items++;
 		visualise();		
 
@@ -67,7 +67,8 @@ void * producer() {
 int main() {
     pthread_t prodThread, consThread;
 	sem_init(&sync, 0, 1);
-	sem_init(&delay_consumer, 0, 0);
+	sem_init(&empty, 0, MAX_BUFFER_SIZE);
+	sem_init(&full, 0, 0);
 	
 	pthread_create(&prodThread, NULL, producer, NULL);
 	pthread_create(&consThread, NULL, consumer, NULL);
@@ -77,8 +78,9 @@ int main() {
 		
 	// print final semaphore values
 	sem_getvalue(&sync, &syncVal);
-	sem_getvalue(&delay_consumer, &delayVal);
-	printf("sSync = %d, sDelayConsumer = %d\n", syncVal, delayVal);
+	sem_getvalue(&empty, &emptyVal);
+	sem_getvalue(&full, &fullVal);
+	printf("sSync = %d, sEmpty = %d, sFull = %d\n", syncVal, emptyVal, fullVal);
 
 	return 0;
 
