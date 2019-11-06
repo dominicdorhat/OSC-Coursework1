ad4a394150196d84ba24b12fd5a38fa90ff90c00
dominicdorhat <johnfruit099@gmail.com>
2019-11-06 15:02:43 +0000

    Modified task3.c to reflect new outputSamples, code review needed

--- task3.c
+++ task3.c
@@ -8,17 +8,14 @@
 #include "coursework.h"
 #include "linkedlist.h"
 
-sem_t sync,full,empty;
+sem_t sync,full,sleep_producer;
 struct element *queue = NULL;
 struct element *queueTail = NULL;
-int items, syncVal, fullVal, emptyVal, produced, consumed;
+int items, syncVal, fullVal, sleep_producerVal, produced, consumed;
 
 void visualise() {
 	struct element *queueCur = NULL; 
 	queueCur = queue;
-	// while (queueCur != NULL ){
-	// 	printf("\t iIndex = %d\n", items);
-	// }
 
 	for(; queueCur != NULL; queueCur = queueCur->pNext) {
 		printf("%c", (char *) (queueCur->pData));
@@ -28,23 +25,27 @@ void visualise() {
 
 // consumer
 void * consumer(void * p) {
-	int consumer_id = *(int *) p; 
 
 	//consumes job
-	for (int i = 0; i < MAX_NUMBER_OF_JOBS/NUMBER_OF_CONSUMERS; i++) {
+	for (int i = 0; i < MAX_NUMBER_OF_JOBS; i++) {
 	// while(1){
 		sem_wait(&full);
 		sem_wait(&sync);		
+
 		// critical section
 		removeFirst(&queue, &queueTail);
 		items--;
 		consumed++;		
-		printf("Consumer id: %d, Produced = %d, Consumed = %d ", consumer_id, produced, consumed);
+		printf("Consumer %d, Produced = %d, Consumed = %d: ", 1, produced, consumed);
 		visualise(); 
 		// end of critical section
+
 		sem_post(&sync);
-		if (i != MAX_NUMBER_OF_JOBS/NUMBER_OF_CONSUMERS - 1) {
-			sem_post(&empty);
+		// if (i != MAX_NUMBER_OF_JOBS/NUMBER_OF_CONSUMERS - 1) {
+		
+		sem_getvalue(&full, &fullVal);
+		if (fullVal == 49) {
+			sem_post(&sleep_producer);
 		}
 		
 	}	
@@ -52,20 +53,26 @@ void * consumer(void * p) {
 
 // producer
 void * producer(void * p) {
-	int producer_id = *(int *) p;
 	char * star = (char *) '*'; // create star
 
 	// produces job
 	for (int i = 0; i < MAX_NUMBER_OF_JOBS; i++) {
 	//while(1){
-		sem_wait(&empty);
-		sem_wait(&sync);	
+		
+		sem_getvalue(&full, &fullVal);
+
+		if(fullVal == 50 ) {
+			sem_wait(&sleep_producer);
+		}		
+		sem_wait(&sync);
+
 		// critical section
         addLast( star, &queue, &queueTail); 
 		items++;
 		produced++;		
-		printf("Producer id: %d, Produced = %d, Consumed = %d ", producer_id, produced, consumed);
+		printf("Producer %d, Produced = %d, Consumed = %d: ", 1, produced, consumed);
 		visualise();
+
 		// end of critical section		
 		sem_post(&sync);
 		sem_post(&full);
@@ -74,33 +81,23 @@ void * producer(void * p) {
 
 int main() {
 
-    pthread_t prodThreadArr[NUMBER_OF_PRODUCERS], consThreadArr[NUMBER_OF_CONSUMERS];
-	int consID[NUMBER_OF_CONSUMERS] = {0,1,2,3};
-	int prodID[NUMBER_OF_PRODUCERS] = {0};
+	pthread_t prodThread, consThread;
 
-	sem_init(&sync, 0, 1);
-	sem_init(&empty, 0, 50); //PRODUCER SLEEPS ON BINARY SEM
-	sem_init(&full, 0, 0);
+	sem_init(&sync, 0, 1); 
+	sem_init(&sleep_producer, 0, 0); // binary sem for sleeping producer
+	sem_init(&full, 0, 0); // counting semaphore
 
-	for (int i = 0; i < NUMBER_OF_PRODUCERS ; i++ ) {
-		pthread_create(&prodThreadArr[i], NULL, producer, (void *) &(prodID[i]) ); 
-	}
+	pthread_create(&prodThread, NULL, producer, NULL); 
+	pthread_create(&consThread, NULL, consumer, NULL );
 
-	for (int i = 0; i < NUMBER_OF_CONSUMERS; i++){
-		pthread_create(&consThreadArr[i], NULL, consumer, (void * ) &(consID[i]) );
-	}
-
-	for (int i = 0; i < NUMBER_OF_PRODUCERS; i++)
-		pthread_join(prodThreadArr[i], NULL);
-	
-	for (int i = 0; i < NUMBER_OF_CONSUMERS; i++)
-		pthread_join(consThreadArr[i], NULL);
+	pthread_join(prodThread, NULL);	
+	pthread_join(consThread, NULL);
 		
 	// print final semaphore values
 	sem_getvalue(&sync, &syncVal);
-	sem_getvalue(&empty, &emptyVal);
+	sem_getvalue(&sleep_producer, &sleep_producerVal);
 	sem_getvalue(&full, &fullVal);
-	printf("sSync = %d, sEmpty = %d, sFull = %d\n", syncVal, emptyVal, fullVal);
+	printf("sSync = %d, sSleepProducer = %d, sFull = %d\n", syncVal, sleep_producerVal, fullVal);
 	
 	return 0;
 
